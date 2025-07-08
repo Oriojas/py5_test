33f9faed3287446944295d01f475896ab8d4800a
Oscar ThinkPad <oscarriojas@gmail.com>
2025-07-08 09:14:07 -0500

    feat/ init repo

---
+++ test_1.py
@@ -0,0 +1,18 @@
+import py5
+import random
+
+
+def setup():
+    py5.size(800, 800)
+    py5.rect_mode(py5.CENTER)
+    py5.frame_rate(500)
+
+
+def draw():
+    if py5.is_mouse_pressed:
+         py5.stroke(py5.color(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+         py5.stroke_weight(random.randint(0, 20))
+         py5.point(py5.mouse_x - random.randint(0, 20) , py5.mouse_y - random.randint(0, 20))
+
+py5.run_sketch()
+  
